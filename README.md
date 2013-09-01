4a6b83737ad506a79364be5328533b6b718abb3c
rwood-moz <rwood@mozilla.com>
2013-08-31 20:41:21 -0400

    Moar progress on rev 0.1

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-awsy
-====
+AWSY (Are We Slim Yet)
+======================
 
-Are We Slim Yet
+The purpose of the AWSY test driver is to automate the execution of B2G emulator tests, with the ultimate goal of detecting Firefox OS memory leaks. The tests themselves are based on the Orangutan (https://github.com/wlach/orangutan) testing tool. This driver will startup the B2G emulator, copy the required files onto the emulator, run the tests and grab B2G memory information using the get_about_memory B2G tool.
+
+Installation
+============
+
+The following will guide you through the AWSY installation (Ubuntu x64).
+
+1) B2G Emulator and Tools:
+
+It is assumed that you already have a B2G build environment setup, with a successful build of the B2G emulator already completed. Also within the standard B2G build folder is the B2G/tools folder, which contains the get_about_memory.py script.
+
+The following environment variable MUST be set and exported in order for AWSY to locate your emulator build and the about_memory script:
+
+B2G_HOME = <path to your B2G build> i.e. /home/rwood/B2G
+
+2) Orangutan:
+
+Please see the instructions on building the Orangutan binary in the readme here:
+
+        https://github.com/wlach/orangutan
+
+Once you have an orangutan binary, the following environment variable must be set and exported to in order for AWSY to lcoate your orangutan binary:
+
+AWSY_ORANG = <path to your orangutan binary> i.e. /home/rwood/awsy/orangutan
+
+3) This test driver:
+
+        git clone https://github.com/rwood-moz/awsy.git
+
+B2G Emulator Prerequisites
+==========================
+Currently some manual setup of the B2G emulator is required, at least until these items are automated in the future, via prerences or build requirements. For now, in order for the AWSY emulator tests to run, the following B2G emulator items must be manually set:
+
+1) The FTU (First Time Use) application must NOT appear on emulator boot-up. Just run the emulator for the first time, and manually run through the FTU app so it doesn't appear on subsequent emulator start-ups.
+
+2) The FirefoxOS phone lock must be disabled so that the emulator/phone doesn't lock up during the sleeps between tests or when grabbing the memory information. Just go into the Settings app on the emulator and choose to disable the phone lock. Also, it is good to set the screen timeout to 'never' so that the screen display will not sleep/turn off.
+
+3) Ensure that the volume warning has already been accepted. To do this, start the FM Radio app on the emulator; the first time this app appears there is a volume warning; click the 'continue' button and then after the radio app starts, just close the app.
+
+Running the Tests
+=================
+
+First ensure installation is complete and the B2G_HOME and AWSY_ORANG environment variables have been set as indicated in the installation section above.
+
+Secondly, startup the emulator manually, and ensure the prerequistes are met as described above, and then SHUT DOWN the emulator before continuing. Ensure that no emulator instances are running before continuing.
+
+Note: Currently DMD processing of emulator DMD memeory dumps doesn't work (Bug 910847). Therefore by default DMD processing is currenlty turned OFF.
+
+Third, open a terminal and start the driver. For example, to run one iteration of the 'runningAppsEmulator' test, with DMD processing turned off, do the following:
+
+        cd awsy
+        python awsy.py tests/runningAppsEmulator.dat
+        
+For more information on the optional command line arguments:
+
+        python awsy.py --help
+
+It currently takes a large amount of time for the get_about_memory tool to pull the GC/cc files off of the emulator; the driver may seem to get stuck at "Got N/N files." Since it takes so long, this has currently been disabled (using the --no-gc-cc-log option).
+
+The resulting about-memory files will be found in the local folder where AWSY was cloned to.
+
--- awsy.py
+++ awsy.py
@@ -12,51 +12,63 @@ from optparse import OptionParser
 
 class AWSY(object):
 
+    emu_proc = 'emulator64-arm'
+
     def __init__(self):
         # Ensure $B2G_HOME is set
         try:
             self.b2g_home = os.environ["B2G_HOME"]
-            print "\n$B2G_HOME points to emulator location at: %s" %self.b2g_home
+            print "\n$B2G_HOME points to: %s" %self.b2g_home
         except:
-            print "$B2G_HOME env var must be set to point to the B2G emulator location."
+            print "\n$B2G_HOME env var must be set to point to the B2G emulator.\n"
             sys.exit(1)
         # Ensure run-emulator script exist
         if not os.path.exists("%s/run-emulator.sh" %self.b2g_home):
-            print("The emulator doesn't exist in the specified $B2G_HOME location.")
+            print("\nThe emulator doesn't exist at the $B2G_HOME location.\n")
             exit(1)
         # Ensure get_about_memory tool script exists
         if not os.path.exists("%s/tools/get_about_memory.py" %self.b2g_home):
-            print("The get_about_memory.py script doesn't exist in $B2G_HOME/tools.")
+            print("\nThe get_about_memory.py script doesn't exist in $B2G_HOME/tools.\n")
+            exit(1)
+        # Ensure $AWSY_ORANG is set
+        try:
+            self.awsy_orang = os.environ["AWSY_ORANG"]
+            print "\n$AWSY_ORANG points to: %s" %self.awsy_orang
+        except:
+            print "\n$AWSY_ORANG env var must be set to point to the orangutan binary.\n"
+            sys.exit(1)
+        # Ensure orang binary exists
+        if not os.path.exists("%s/orng" %self.awsy_orang):
+            print("\nThe orangutan binary doesn't exist at the $AWSY_ORANG location.\n")
             exit(1)
 
     def backup_existing_reports(self):
         # If any about-memory reports exist, back them up
-        file_path = "%s/tools" %self.b2g_home
-        print "\nSearching for existing memory reports in %s" % file_path
-        entire_file_list = os.listdir(file_path)
+        #file_path = "./" %self.b2g_home
+        print "\nBacking up any existing memory reports in %s" %os.getcwd()
+        entire_file_list = os.listdir(os.getcwd())
         for found_file in entire_file_list:
-            if found_file.startswith("memory-report"):
-                print "\nFound existing memory-report, renaming..."
+            if found_file.startswith("about-memory"):
                 try:
-                    os.rename(file_path + "/" + found_file, file_path + "/" + "old_" + found_file)
+                    cur_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+                    os.rename(found_file, "old_%s_%s" %(cur_time, found_file))
                 except:
-                    print "Unable to rename existing memory report: %s" %found_file
+                    print "Unable to rename existing memory report: %s" %found_file    
 
     def start_emu(self):
         # Startup the B2G emulator; location specified by $B2G_HOME
         print "\nStarting the B2G emulator..."
-        return_code = subprocess.call(["$B2G_HOME/run-emulator.sh"], shell=True)
-        if return_code:
-            print "\nFailed to startup the B2G emulator. Ensure $B2G_HOME points to the emulator location."
-            sys.exit(1)
-        time.sleep(60)
+        # Want emulator to start in own process but don't want this parent to wait for it to finish
+        os.system("gnome-terminal -e $B2G_HOME/run-emulator.sh")
+        # Sleep for emulator bootup
+        # <TODO> Use adb wait for device instead of a static sleep??
+        time.sleep(120)
 
         # Verify emulator is running
-        proc = 'emulator64-arm'
         returned = os.popen("ps -Af").read()
-        found = returned.count(proc)
+        found = returned.count(self.emu_proc)
         if found == 0:
-            print("\nThe B2G emulator failed to start; process not found.")   
+            print("\nThe B2G emulator failed to start; process not found.")
             sys.exit(1)
 
         # ADB forward to the emulator
@@ -65,17 +77,34 @@ class AWSY(object):
             print "\nFailed to forward adb port to the emulator."
             sys.exit(1)
 
+        # **** Important note ****
+        # In order for the tests to run, currently the following are prerequisites and assumed
+        # are true at the time of the b2g emulator startup. Set this in the build?!
+        # 1) The FTU app is DISABLED so it won't start on emulator startup
+        # 2) The lock-screen is DISABLED so the emulator screen won't ever lock
+        # 3) The volume warning has already been accepted (fmradio app); maybe change test to click continue
+
     def delete_old_reports_from_emu(self):
         # Ensure there are no memory reports on the emulator (/data/local/tmp) left from a previous run
-        print "\nEnsuring there are no memory-reports on the emulator from a previous run..."
-        print "\n<TO-DO> ***** just delete the /tmp/memory-reports folder from the emulator via adb shell *****"
+        print "\nRemoving any previous memory reports from the emulator..."
+        try:
+            subprocess.call(["rm -r /data/local/tmp/memory-reports"], shell=True)
+        except:
+            pass
 
-    def copy_test_onto_emu(self, orangutan_test):
-        # Copy the given test script onto the emulator in /data/local
-        print '\nCopying orangutan test onto the emulator: %s' %orangutan_test
-        return_code = subprocess.call(["adb push %s /data/local/%s" %(orangutan_test, orangutan_test)], shell=True)
+    def copy_file_onto_emu(self, file_name):
+        # Make sure the file has exe permissions first
+        try:
+            subprocess.call(["chmod 777 %s" %file_name], shell=True)
+        except:
+            pass
+
+        # Copy the given file onto the emulator in /data/local
+        print '\nCopying file onto the emulator: %s' %file_name
+        return_code = subprocess.call(["adb push %s /data/local/%s" %(file_name, file_name)], shell=True)
+        time.sleep(5)
         if return_code:
-            print "\nFailed to copy orangutan test script onto the emulator."
+            print "\nFailed to copy file onto the emulator."
             sys.exit(1)
 
     def get_memory_report(self, dmd):
@@ -85,15 +114,15 @@ class AWSY(object):
             return_code = subprocess.call(["$B2G_HOME/get_about_memory.py"], shell=True)
         else:
             print "\nGetting about_memory report without DMD..."
-            return_code = subprocess.call(["$B2G_HOME/get_about_memory.py -n"], shell=True)
+            return_code = subprocess.call(["$B2G_HOME/tools/get_about_memory.py --no-dmd --no-auto-open --no-gc-cc-log"], shell=True)
         if return_code:
             print "\nFailed to get memory report."
             sys.exit(1)
 
     def run_test(self, orangutan_test, cur_iteration, iterations):
-        # Run the test one cycle; assuming test and orng already exist on emulator in /data/local
-        print "\nStarting '%s' iteration %d of %d..." %(orangutan_test, cur_iteration, iterations)
-        return_code = subprocess.call(["adb shell /data/local/orng /dev/input/event0 /data/local/%s" %orangutan_test], shell=True)
+        # Run the test one cycle; assuming test and orng already exist on emulator in /data/local/
+        print "\nRunning '%s' iteration %d of %d..." %(orangutan_test, cur_iteration, iterations)
+        return_code = subprocess.call(["adb shell /data/local/orangutan/orng /dev/input/event0 /data/local/%s" %orangutan_test], shell=True)
         if return_code:
             print "\nFailed to run the orangutan test."
             sys.exit(1)
@@ -102,14 +131,29 @@ class AWSY(object):
         # Actually drive the tests
         for cur_iteration in range(1, iterations + 1):
             self.run_test(orangutan_test, cur_iteration, iterations)
+            print "\nIteration complete, sleeping for %d seconds..." %sleep
             time.sleep(sleep)
             # TODO: CHeck for nap
             # TODO: Check for checkpoint
 
+    def kill_emulator(self):
+        # Tests are finished, kill the emulator
+        print "\nKilling the emulator instance..."
+        try:
+            returned = os.popen("ps -Af").read()
+            process_list = returned.split("\n")
+            for i, s in enumerate(process_list):
+                if self.emu_proc in s:
+                    proc_details = process_list[i].split()
+                    emu_pid = int(proc_details[1])
+                    os.kill(emu_pid, 9)
+        except:
+            # Failed to kill emulator
+            print "\nCould'nt kill the emulator process."
 
 class awsyOptionParser(OptionParser):
     def __init__(self, **kwargs):
-        OptionParser.__init__(self, **kwargs)       
+        OptionParser.__init__(self, **kwargs)
         self.add_option('--iterations',
                         action='store',
                         dest='iterations',
@@ -175,23 +219,25 @@ def cli():
     print "After every %d iterations take a nap for %d seconds." %(options.nap_after, options.nap_time)
     print "Get additional about_memory dumps after every %d iterations." %options.checkpoint_every
     if options.dmd:
-        print "DMD will be included in the memory dumps.\n"
+        print "DMD will be included in the memory dumps."
     else:
-        print "DMD will not be included in the memory dumps.\n"
+        print "DMD will NOT be included in the memory dumps."
 
-    print "Starting in 60 seconds..."
-    time.sleep(60)
-
-    # Create our test runner and begin
+    # Create our test runner
     awsy = AWSY()
+    print "\nStarting in 30 seconds..."
+    time.sleep(30)
+
+    # Begin
     awsy.backup_existing_reports()
     awsy.start_emu()
+    awsy.copy_file_onto_emu('orangutan/orng')
     awsy.delete_old_reports_from_emu()
-    awsy.copy_test_onto_emu(test_name)
+    awsy.copy_file_onto_emu(test_name)
     awsy.get_memory_report(options.dmd)
 
     # Actually run the test cycle(s)
-    awsy.drive(orangutan_test,
+    awsy.drive(test_name,
                options.iterations,
                options.sleep_between,
                options.nap_after,
@@ -200,7 +246,9 @@ def cli():
                options.dmd)
 
     # Get the final memory report
-    awsy.get_memory_report()
+    awsy.get_memory_report(options.dmd)
+
+    awsy.kill_emulator()
     print "\nFinished."
 
 
